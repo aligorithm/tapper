8037eed936aa9dfc553db19d5efedd15d5fc0272
Aliyu Yisa <aliyisa@outlook.com>
2020-05-29 18:06:02 +0100

    remove provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:tapper/home.dart';
-import 'package:tapper/provider.dart';
 
 class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => TapperProvider())],
-      child: MaterialApp(
-        title: 'Tapper',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        home: Home(),
+    return MaterialApp(
+      title: 'Tapper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
+      home: Home(),
     );
   }
 }
